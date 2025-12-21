eeec190efe382052e328b2b93a18797d17ad80d2
rois90 <roissm90@gmail.com>
2025-12-21 01:23:33 +0100

    eight commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
@@ -15,49 +15,53 @@ const io = new Server(server, {
 const PORT = process.env.PORT || 3000;
 
 // Ruta de prueba
-app.get('/', (req, res) => {
-  res.send('¡Servidor funcionando!');
+app.get("/", (req, res) => {
+  res.send("¡Servidor funcionando!");
 });
 
 // Usuarios conectados: socket.id => username
 const usuarios = {};
 
-io.on('connection', (socket) => {
-  console.log('Usuario conectado:', socket.id);
+io.on("connection", (socket) => {
+  console.log("Usuario conectado:", socket.id);
 
   // Usuario se une al chat
-  socket.on('join', ({ username }) => {
+  socket.on("join", ({ username }) => {
     usuarios[socket.id] = username;
     console.log(`${username} se ha unido al chat`);
 
     // Avisar a todos menos a él que alguien se unió
-    socket.broadcast.emit('user-joined', { username });
+    socket.broadcast.emit("user-joined", { username });
 
     // Enviar lista actualizada a todos
-    io.emit('usuarios-conectados', Object.values(usuarios));
+    io.emit("usuarios-conectados", Object.values(usuarios));
   });
 
   // Usuario envía un mensaje
-  socket.on('mensaje', (msg) => {
-    const username = usuarios[socket.id] || 'Anon';
-    io.emit('mensaje', { user: username, text: msg });
+  socket.on("mensaje", (msg) => {
+    const username = usuarios[socket.id] || "User";
+    const mensajeObj = {
+      user: username,
+      text: msg,
+      timestamp: Date.now(),
+    };
+    io.emit("mensaje", mensajeObj);
   });
 
   // Usuario se desconecta
-  socket.on('disconnect', () => {
-    const username = usuarios[socket.id] || 'Anon';
-    console.log('Usuario desconectado:', username);
+  socket.on("disconnect", () => {
+    const username = usuarios[socket.id] || "Anon";
+    console.log("Usuario desconectado:", username);
     delete usuarios[socket.id];
 
     // Actualizar lista de usuarios conectados
-    io.emit('usuarios-conectados', Object.values(usuarios));
+    io.emit("usuarios-conectados", Object.values(usuarios));
 
     // Avisar a los demás que alguien se fue
-    socket.broadcast.emit('user-left', { username });
+    socket.broadcast.emit("user-left", { username });
   });
 });
 
 server.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
-
